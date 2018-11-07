b41eb8bf492295e9566dce610bc7722a25d13331
Jeremy Ellis <keyfreemusic@gmail.com>
2018-11-06 22:32:41 -0800

    Create kera01.sh

---
+++ kera01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+source ~/virtual-tf/bin/activate
+
+
+
+
+echo "running python hello42.py from the rocksetta-examples folder"
+echo ""
+echo ""
+
+python kera01.py
+
+echo ""
+echo "--------------------Done--------------------------"
